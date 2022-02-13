be0d005c2510b1c9bae4d7d68ba05e49a6b44720
Conslatekoyo <conslatekoyo@gmail.com>
2022-02-13 12:58:01 +0300

    assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+fun main() {
+    var studentName = arrayOf("Conslate Koyo", "Zilper Mwangi", "Shiroh Chepkemoi", "Florence Nyambura")
+    var studentAge = arrayOf("21 yrs old", "16 yrs old", "20 yrs old", "25 yrs old")
+    var studentNumber = arrayOf("o7o9897865", "0757845326", "0704005678", "0732227785")
+    var studentWeight = arrayOf("58kg", "47kg", "65kg","80kg")
+    var kenyan = arrayOf( true, false, true, true,)
+
+
+    println(studentName[0])
+    println(studentAge[0])
+    println(studentNumber[0])
+    println(studentWeight[0])
+    println(kenyan[0])
+    println(" ")
+
+    println(studentName[1])
+    println(studentAge[1])
+    println(studentNumber[1])
+    println(studentWeight[1])
+    println(kenyan[1])
+    println("")
+
+    println(studentName[2])
+    println(studentAge[2])
+    println(studentNumber[2])
+    println(studentWeight[2])
+    println(kenyan[2])
+    println("")
+
+    println(studentName[3])
+    println(studentAge[3])
+    println(studentNumber[3])
+    println(studentWeight[3])
+    println(kenyan[3])
+    println("")
+
+}
